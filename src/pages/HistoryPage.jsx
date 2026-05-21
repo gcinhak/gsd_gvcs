9ab1ec1f11485ae8289422446b88437cfa69632d
inhak <inhak@gvcs-mg.org>
2026-05-21 16:53:05 +0900

    TEST

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -4,6 +4,9 @@ import CampusBadge from '../components/CampusBadge';
 import { HISTORY, CAMPUS_COLORS, EVENT_ORDER } from '../data';
 
 // 윤지 공주가 수정함.
+// git add .
+// git commit -m "역대 전적 페이지 구현 (2024 데이터까지)"
+// git push origin main
 
 function eventOrderIdx(name) {
     const idx = EVENT_ORDER.indexOf(name);
