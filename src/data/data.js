15d577ea25380ca3d817057a80b0f6dc14e7622d
inhak <inhak@gvcs-mg.org>
2026-05-26 16:59:15 +0900

    남자 중등 미정 vs 미정으로 수정

--- src/data/data.js
+++ src/data/data.js
@@ -1014,7 +1014,7 @@ export const LIVE_MATCHES = [
         round: '예선',
         category: '남자 중등',
         venue: '체육관',
-        teams: { home: '문경', away: '미정' },
+        teams: { home: '미정', away: '미정' },
         lineup: { home: [], away: [] },
     },
     {
